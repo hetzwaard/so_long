f8a95681c4ed76126e2bf0fd7a27f67724e7c700
hetzwaard <mahmutsedatkilic@gmail.com>
2025-05-08 16:35:50 +0200

    cannot read png

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mahkilic <mahkilic@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/04/07 13:24:23 by mahkilic      #+#    #+#                 */
-/*   Updated: 2025/05/08 16:12:41 by mahkilic      ########   odam.nl         */
+/*   Updated: 2025/05/08 16:30:52 by mahkilic      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	main(int argc, char **argv)
 	map.map = map_read(open(argv[1], O_RDONLY), map_line(fd));
 	map_init(&map);
 	flood_fill(&map);
-	map.mlx = mlx_init(map.x * 128, map.y * 128, "so_long", false);
+	map.mlx = mlx_init(map.x * 96, map.y * 96, "so_long", false);
 	if (!map.mlx)
 		error_exit(2);
 	map_build(&map);
