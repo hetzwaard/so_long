24130ba301379ba5feb0b497e5931a419eab3301
Mahmut Kilic <mahkilic@f0r3s26.codam.nl>
2025-05-10 19:34:37 +0200

    update

--- src/game/collect_exit.c
+++ src/game/collect_exit.c
@@ -12,18 +12,18 @@
 
 #include "../../include/so_long.h"
 
-void	check_exit(t_map *map, int x, int y)
+void	set_space(t_map *map, int x, int y)
 {
 	x /= 96;
 	y /= 96;
 	map->map[y][x] = '0';
 }
 
-int	check_collect(t_map *map, char collect, int x, int y)
+int	check_move(t_map *map, char move, int x, int y)
 {
 	x += map->player->instances[0].x / 96;
 	y += map->player->instances[0].y / 96;
-	if (map->map[y][x] == collect)
+	if (map->map[y][x] == move)
 		return (1);
 	return (0);
 }
@@ -34,20 +34,20 @@ void	collect_exit(t_map *map)
 
 	if (map->c_count == 0)
 	{
-		if (check_collect(map, 'E', 0, 0))
+		if (check_move(map, 'E', 0, 0))
 		{
 			map->player->instances[0].enabled = false;
 			mlx_close_window(map->mlx);
 		}
 	}
-	else if (check_collect(map, 'C', 0, 0))
+	else if (check_move(map, 'C', 0, 0))
 	{
 		i = 0;
 		while (map->player->instances[0].x != map->collect->instances[i].x
 			|| map->player->instances[0].y != map->collect->instances[i].y)
 			i++;
 		map->collect->instances[i].enabled = false;
-		check_exit(map, map->collect->instances[i].x,
+		set_space(map, map->collect->instances[i].x,
 			map->collect->instances[i].y);
 		map->c_count--;
 	}
